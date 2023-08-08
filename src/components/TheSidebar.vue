cbf6d46a8869a3c88274a0785adc5c25710f9d31
munna <munna@msdmunna.@gmail.com>
2023-08-08 13:23:23 +0600

    file modified

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -4,5 +4,161 @@
     </aside>
 </template>
 
+<script>
+export default {
+    
+}
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+aside {
+    display: flex;
+    flex-direction: column;
+
+    background-color: var(--dark);
+    color: var(--light);
+
+    width: calc(2rem + 32px);
+    overflow: hidden;
+    min-height: 100vh;
+    padding: 1rem;
+
+    transition: 0.2s ease-in-out;
+
+    .flex {
+        flex: 1 1 0%;
+    }
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img {
+            width: 2rem;
+        }
+    }
+
+    .menu-toggle-wrap {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+
+        position: relative;
+        top: 0;
+        transition: 0.2s ease-in-out;
+
+        .menu-toggle {
+            transition: 0.2s ease-in-out;
+
+            .material-icons {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                .material-icons {
+                    color: var(--primary);
+                    transform: translateX(0.5rem);
+                }
+            }
+        }
+    }
+
+    h3,
+    .button .text {
+        opacity: 0;
+        transition: opacity 0.3s ease-in-out;
+    }
+
+    h3 {
+        color: var(--grey);
+        font-size: 0.875rem;
+        margin-bottom: 0.5rem;
+        text-transform: uppercase;
+    }
+
+    .menu {
+        margin: 0 -1rem;
+
+        .button {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+
+            transition: 0.2s ease-in-out;
+            padding: 0.5rem 1rem;
+
+            .material-icons {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-in-out;
+            }
+
+            .text {
+                color: var(--light);
+                transition: 0.2s ease-in-out;
+            }
+
+            &:hover {
+                background-color: var(--dark-alt);
+
+                .material-icons,
+                .text {
+                    color: var(--primary);
+                }
+            }
+
+            &.router-link-exact-active {
+                background-color: var(--dark-alt);
+                border-right: 5px solid var(--primary);
+
+                .material-icons,
+                .text {
+                    color: var(--primary);
+                }
+            }
+        }
+    }
+
+    .footer {
+        opacity: 0;
+        transition: opacity 0.3s ease-in-out;
+
+        p {
+            font-size: 0.875rem;
+            color: var(--grey);
+        }
+    }
+
+    &.is-expanded {
+        width: var(--sidebar-width);
+
+        .menu-toggle-wrap {
+            top: -3rem;
+
+            .menu-toggle {
+                transform: rotate(-180deg);
+            }
+        }
+
+        h3,
+        .button .text {
+            opacity: 1;
+        }
+
+        .button {
+            .material-icons {
+                margin-right: 1rem;
+            }
+        }
+
+        .footer {
+            opacity: 0;
+        }
+    }
+
+    @media (max-width: 1024px) {
+        position: absolute;
+        z-index: 99;
+    }
+}</style>
