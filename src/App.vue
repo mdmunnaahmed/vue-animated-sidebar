f6ecba50860da9ca5b3a8ef2f6c465cc07b52318
munna <munna@msdmunna.@gmail.com>
2023-08-08 12:49:20 +0600

    sidebar init

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="app">
+    <the-sidebar></the-sidebar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheSidebar from './components/TheSidebar.vue';
+export default {
+  component: {
+    TheSidebar
+  }
+}
+</script>
+
+<style lang="scss">
+:root {
+  --primary: #4ade80;
+  --primary-alt: #22c55e;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira sans', sans-serif;
+}
+
+body {
+  color: var(--light);
+  background: var(--dark);
+}
+
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width: 1024px) {
+      padding-left: 6rem;
+    }
+  }
+}
+</style>
---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,8 @@
+<template>
+    <aside>
+        <h2>SideBar</h2>
+    </aside>
+</template>
+
+
+<style lang="scss" scoped></style>
